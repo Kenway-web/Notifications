6c8ae8b3f9b83ed1661b52cf4bd538e33b5c8d64
Shoaib Ali <shoaibali26021999@gmail.com>
2022-09-11 15:14:14 +0530

    Actions added inside the notification

--- app/src/main/java/com/example/notifcationdemo/MainActivity.kt
+++ app/src/main/java/com/example/notifcationdemo/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.notifcationdemo
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ClipDescription
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,18 +16,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
-    private val channelID="com.example.notifcationdemo.channel1"
+    private val channelID = "com.example.notifcationdemo.channel1"
 
     // defining notification manager instance , require for notification instance
 
-    private var notificationManager:NotificationManager? = null
+    private var notificationManager: NotificationManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        createNotificationChannel(channelID,"DemmoChannel","this is a demo ")
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelID, "DemmoChannel", "this is a demo ")
         button.setOnClickListener {
             displayNotification()
         }
@@ -33,26 +35,69 @@ class MainActivity : AppCompatActivity() {
     }
 
     //diplays a icon title and description
-    private fun displayNotification()
-    {
-        val notificationId=45
-        val notification=NotificationCompat.Builder(this@MainActivity,channelID)
-                        .setContentTitle("Demo Title")
-                        .setContentText("This is a demo notification")
-                        .setSmallIcon(android.R.drawable.ic_dialog_info)
-                        .setAutoCancel(true)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .build()
-        notificationManager?.notify(notificationId,notification)
+    private fun displayNotification() {
+        val notificationId = 45
+
+        // intent to launch user activity
+
+        val tapResultIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT // if system creates new intent and pendingIntenet exist it keeps the old intent but replace the data with new intenet
+
+
+        )
+
+        // action Button
+
+        val intent3 = Intent(this, SettingsActivity::class.java)
+        val pendingIntent3: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent3,
+            PendingIntent.FLAG_UPDATE_CURRENT // if system creates new intent and pendingIntenet exist it keeps the old intent but replace the data with new intenet
+        )
+
+        val action3: NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0,"Settings",pendingIntent3).build()
+
+
+        // action Button 2
+
+        val intent2 = Intent(this, DetailsActivity::class.java)
+        val pendingIntent2: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent2,
+            PendingIntent.FLAG_UPDATE_CURRENT // if system creates new intent and pendingIntenet exist it keeps the old intent but replace the data with new intenet
+        )
+
+        val action2: NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0,"Details",pendingIntent2).build()
+
+
+
+        val notification = NotificationCompat.Builder(this@MainActivity, channelID)
+            .setContentTitle("Demo Title")
+            .setContentText("This is a demo notification")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent) // setting intent
+            .addAction(action2)
+            .addAction(action3)
+            .build()
+        notificationManager?.notify(notificationId, notification)
 
     }
 
-    private fun createNotificationChannel(id:String, name:String, channelDescription: String){
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
-        {
-            val importance=NotificationManager.IMPORTANCE_HIGH
-            val channel= NotificationChannel(id,name,importance).apply {
-                description=channelDescription
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
             }
             notificationManager?.createNotificationChannel(channel)
 
